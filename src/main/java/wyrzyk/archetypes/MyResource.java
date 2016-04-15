f014b8ea97c597938758d67cf6c8f5b66fdd4697
Michał Wyrzykowski <michal.wyrzykowski@spartez.com>
2016-04-15 07:20:42 +0200

    added lifecycle resource

--- src/main/java/wyrzyk/archetypes/MyResource.java
+++ src/main/java/wyrzyk/archetypes/MyResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -18,8 +19,8 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    @Produces(MediaType.TEXT_HTML)
+    public Response getIt() {
+        return Response.ok("Got it!").build();
     }
 }
