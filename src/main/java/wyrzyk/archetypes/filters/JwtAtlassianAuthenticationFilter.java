73b4f6ff5c48c2f279c38d1c3a25f5cf871b43dd
Michał Wyrzykowski <michal.wyrzykowski@spartez.com>
2016-04-19 17:06:11 +0200

    configured logs (console appender)

--- src/main/java/wyrzyk/archetypes/filters/JwtAtlassianAuthenticationFilter.java
+++ src/main/java/wyrzyk/archetypes/filters/JwtAtlassianAuthenticationFilter.java
@@ -1,13 +1,17 @@
 package wyrzyk.archetypes.filters;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
+@Slf4j
 public class JwtAtlassianAuthenticationFilter implements Filter {
 
 
@@ -18,6 +22,8 @@ public class JwtAtlassianAuthenticationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        final HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        log.debug("filtering request {} : {}", httpServletRequest.getMethod(), httpServletRequest.getServletPath());
         chain.doFilter(request, response);
     }
 
