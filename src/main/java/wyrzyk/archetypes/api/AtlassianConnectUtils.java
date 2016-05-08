82949152fc1a89040a0da819d55af541cb0968a4
Michał Wyrzykowski <michal.wyrzykowski@spartez.com>
2016-05-08 14:00:11 +0200

    changed atlassian connect utils interface to UtilityClass

--- src/main/java/wyrzyk/archetypes/api/AtlassianConnectUtils.java
+++ src/main/java/wyrzyk/archetypes/api/AtlassianConnectUtils.java
@@ -1,14 +1,16 @@
 package wyrzyk.archetypes.api;
 
+import lombok.experimental.UtilityClass;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public interface AtlassianConnectUtils {
-    static HandlerMethodArgumentResolver createClientDtoResolver() {
+@UtilityClass
+public class AtlassianConnectUtils {
+    public static HandlerMethodArgumentResolver createClientDtoResolver() {
         return new ClientInfoDtoResolver();
     }
 
-    static HandlerInterceptor createJwtAuthenticationInterceptor(){
+    public static HandlerInterceptor createJwtAuthenticationInterceptor() {
         return new JwtAuthenticationInterceptor();
     }
 }
